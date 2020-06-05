d18c634b0d62621583918f6fe4ffb2417d9236d6
Mohit Uniyal <mohituniyal2010@gmail.com>
2020-06-05 22:56:52 +0530

    add adv python

---
+++ session 3/customname.py
@@ -0,0 +1,14 @@
+def hello_to_all(name):
+    """
+    This function accepts a string name, and return greeting to all
+    """
+    
+    return (f"{name} says hello to all")
+    
+def add(a,b):
+    return a+b
+
+def print_name():
+    print(__name__)
+    
+print(__name__)
